47abbf26b7a4affd440661047c31a4e8186f00dd
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2017-10-13 22:09:50 +0200

    [temp] Demo palette

--- src/OpenLoco/src/Windows/TitleLogo.cpp
+++ src/OpenLoco/src/Windows/TitleLogo.cpp
@@ -1,24 +1,16 @@
 #include "Graphics/Colour.h"
 #include "Graphics/ImageIds.h"
 #include "Graphics/SoftwareDrawingEngine.h"
+#include "Graphics/TextRenderer.h"
 #include "OpenLoco.h"
 #include "Ui/Widget.h"
 #include "Ui/WindowManager.h"
 
 namespace OpenLoco::Ui::Windows::TitleLogo
 {
-    static constexpr Ui::Size kWindowSize = { 298, 170 };
-
-    namespace Widx
-    {
-        enum
-        {
-            logo
-        };
-    }
+    static constexpr Ui::Size kWindowSize = { 16 * 16, 16 * 16 };
 
     static constexpr Widget _widgets[] = {
-        makeWidget({ 0, 0 }, kWindowSize, WidgetType::wt_3, WindowColour::primary),
         widgetEnd(),
     };
 
@@ -34,35 +26,32 @@ namespace OpenLoco::Ui::Windows::TitleLogo
             getEvents());
 
         window->setWidgets(_widgets);
-        window->enabledWidgets = 1 << Widx::logo;
-
-        window->initScrollWidgets();
-
-        window->setColour(WindowColour::primary, AdvancedColour(Colour::grey).translucent());
-        window->setColour(WindowColour::secondary, AdvancedColour(Colour::grey).translucent());
-
         return window;
     }
 
     // 0x00439298
     static void draw(Ui::Window& window, Gfx::DrawingContext& drawingCtx)
     {
-        drawingCtx.drawImage(window.x, window.y, ImageIds::locomotion_logo);
-    }
-
-    // 0x004392AD
-    static void onMouseUp([[maybe_unused]] Window& window, WidgetIndex_t widgetIndex)
-    {
-        switch (widgetIndex)
+        for (int y = 0; y < 16; y++)
         {
-            case Widx::logo:
-                About::open();
-                break;
+            for (int x = 0; x < 16; x++)
+            {
+                auto _x = window.x + x * 16;
+                auto _y = window.y + y * 16;
+                auto colour = x + y * 16;
+
+                drawingCtx.fillRect(_x, _y, _x + 16, _y + 16, colour, Gfx::RectFlags::none);
+
+                auto* buffer = StringManager::getString(StringIds::buffer_2039);
+                sprintf(const_cast<char*>(buffer), "%02X", colour);
+
+                auto tr = Gfx::TextRenderer(drawingCtx);
+                tr.drawStringCentred(Ui::Point(_x + 8, _y + 3), Colour::white, StringIds::buffer_2039);
+            }
         }
     }
 
     static constexpr WindowEventList kEvents = {
-        .onMouseUp = onMouseUp,
         .draw = draw,
     };
 
